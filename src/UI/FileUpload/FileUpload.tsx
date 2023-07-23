c4a5b212d63643dc8df03dee439f6915d046b1d4
Jesper Nielsen <sermyagin.abcen7@gmail.com>
2023-07-24 01:14:58 +0300

    Added structure of FileUpload component

---
+++ src/UI/FileUpload/FileUpload.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import styles from './FileUpload.module.scss'
+
+const FileUpload: FC = () => {
+  return (
+    <div className={styles.file_upload}>
+
+    </div>
+  )
+}
+
+export default FileUpload
