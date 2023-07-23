22070ba154a591b33ea629115303eb2b6a684d5e
Jesper Nielsen <sermyagin.abcen7@gmail.com>
2023-07-24 01:07:57 +0300

    Added NavLink for HeaderLogo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react'
 import styles from './Header.module.scss'
 import { Logo } from '../../UI/Logo'
+import { NavLink } from 'react-router-dom'
+import routePaths from '../CoreRouter/routePaths.ts'
 const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__icon}>
-        <Logo
-          width={"48px"}
-        />
+        <NavLink to={routePaths.DASHBOARD}>
+          <Logo
+            width={"48px"}
+          />
+        </NavLink>
       </div>
     </header>
   )
