81a78e3b42262849caabf70303d5ee3c3d1fcdc8
Jesper Nielsen <sermyagin.abcen7@gmail.com>
2023-07-31 13:12:47 +0300

    Some changes at style.scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,31 @@
 import { FC } from 'react'
 import styles from './Header.module.scss'
 import { Logo } from '../../UI/Logo'
-import { NavLink } from 'react-router-dom'
-import routePaths from '../CoreRouter/routePaths.ts'
+import { generatePath, NavLink } from 'react-router-dom'
+import { RoutePaths } from '../CoreRouter'
 
 const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <NavLink
-        to={routePaths.DASHBOARD}
-        style={{
-          textDecoration: 'none'
-        }}
-      >
-        <div className={styles.header__logo}>
-          <Logo
-            width={'48px'}
-          />
-          <p className={styles.header__logo__title}>CloudSafeKeep</p>
-        </div>
-      </NavLink>
+      <div className={styles.header__wrapper}>
+        <NavLink
+          to={generatePath(
+            RoutePaths.DASHBOARD,
+            { place: '' },
+          )}
+          style={{
+            textDecoration: 'none',
+          }}
+        >
+          <div className={styles.header__wrapper__logo}>
+            <Logo
+              width={'48px'}
+            />
+            <p className={styles.header__wrapper__logo__title}>CloudSafeKeep</p>
+          </div>
+        </NavLink>
+      </div>
+
     </header>
   )
 }
