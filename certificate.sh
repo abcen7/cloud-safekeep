a166da0b0d3da6881d21057e4d807afa00947e05
Jesper Nielsen <sermyagin.abcen7@gmail.com>
2023-08-08 16:00:41 +0300

    Changed workflows

--- certificate.sh
+++ certificate.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-DOMAIN=cloud.bivreost.su
-EMAIL=sermyagin.abcen7@gmail.com
-
+# Loading env
+# shellcheck disable=SC2039
+source .env
 
 # Start Nginx
 nginx -g 'daemon off;' &
 
 # Obtain or renew SSL certificate using certbot
-certbot certonly --webroot -w /usr/share/nginx/html -d $DOMAIN --email $EMAIL --agree-tos --non-interactive --force-renewal
+certbot certonly --webroot -w /usr/share/nginx/html -d "$DOMAIN" --email "$EMAIL" --agree-tos --non-interactive --force-renewal
 
 # Reload Nginx to use the new certificate
 nginx -s reload
